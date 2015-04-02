7c5bc43a7be9e12dc30cbc33b8ca5140cc042359
Micadurp <neoswed@hotmail.com>
2015-04-02 09:04:44 +0200

    forgot to add the new files

---
+++ Game/Include/System.h
@@ -0,0 +1,54 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+///////////////////////////////
+// PRE-PROCESSING DIRECTIVES //
+///////////////////////////////
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+
+
+//move this
+const bool FULL_SCREEN = false;
+
+class System
+{
+private:
+	int screenWidth;
+	int screenHeight;
+
+	LPCWSTR applicationName;
+	HINSTANCE hinstance;
+	HWND hwnd;
+
+public:
+	System();
+	virtual ~System();
+
+	bool Initialize();
+	void Shutdown();
+	void Run();
+
+	LRESULT CALLBACK MessageHandler(HWND, UINT, WPARAM, LPARAM);
+
+private:
+	bool Frame();
+	void InitializeWindows(int&, int&);
+	void ShutdownWindows();
+
+
+};
+
+/////////////////////////
+// FUNCTION PROTOTYPES //
+/////////////////////////
+static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+
+/////////////
+// GLOBALS //
+/////////////
+static System* ApplicationHandle = 0;
+
+#endif
---
+++ Game/Source/System.cpp
@@ -0,0 +1,218 @@
+#include "System.h"
+
+System::System()
+{
+}
+
+System::~System()
+{
+}
+
+bool System::Initialize()
+{
+	int screenWidth, screenHeight;
+	bool result;
+
+	// Initialize the width and height of the screen to zero before sending the variables into the function.
+	screenWidth = 0;
+	screenHeight = 0;
+
+	// Initialize the windows api.
+	InitializeWindows(screenWidth, screenHeight);
+
+	return true;
+}
+
+void System::Shutdown()
+{
+	// Shutdown the window.
+	ShutdownWindows();
+}
+
+void System::Run()
+{
+	MSG msg;
+	bool done, result;
+
+
+	// Initialize the message structure.
+	ZeroMemory(&msg, sizeof(MSG));
+
+	// Loop until there is a quit message from the window or the user.
+	done = false;
+	while (!done)
+	{
+		// Handle the windows messages.
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		// If windows signals to end the application then exit out.
+		if (msg.message == WM_QUIT)
+		{
+			done = true;
+		}
+		else
+		{
+			// Otherwise do the frame processing.
+			result = Frame();
+			if (!result)
+			{
+				done = true;
+			}
+		}
+
+		// Check if the user pressed escape and wants to quit.
+		/*if (input->IsEscapePressed() == true)
+		{
+			done = true;
+		}*/
+	}
+
+	return;
+
+}
+
+bool System::Frame()
+{
+	return true;
+}
+
+LRESULT CALLBACK System::MessageHandler(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam)
+{
+	return DefWindowProc(hwnd, umsg, wparam, lparam);
+}
+
+void System::InitializeWindows(int& screenWidth, int& screenHeight)
+{
+	WNDCLASSEX wcex;
+	DEVMODE dmScreenSettings;
+	int posX, posY;
+
+
+	// Get an external pointer to this object.
+	ApplicationHandle = this;
+
+	// Get the instance of this application.
+	hinstance = GetModuleHandle(NULL);
+
+	// Give the application a name.
+	applicationName = L"Durp";
+
+	// Setup the windows class with default settings.
+	wcex.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hinstance;
+	wcex.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+	wcex.hIconSm = wcex.hIcon;
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = applicationName;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	// Register the window class.
+	RegisterClassEx(&wcex);
+
+	// Determine the resolution of the clients desktop screen.
+	screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+	// Setup the screen settings depending on whether it is running in full screen or in windowed mode.
+	if (FULL_SCREEN)
+	{
+		// If full screen set the screen to maximum size of the users desktop and 32bit.
+		memset(&dmScreenSettings, 0, sizeof(dmScreenSettings));
+		dmScreenSettings.dmSize = sizeof(dmScreenSettings);
+		dmScreenSettings.dmPelsWidth = (unsigned long)screenWidth;
+		dmScreenSettings.dmPelsHeight = (unsigned long)screenHeight;
+		dmScreenSettings.dmBitsPerPel = 32;
+		dmScreenSettings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+
+		// Change the display settings to full screen.
+		ChangeDisplaySettings(&dmScreenSettings, CDS_FULLSCREEN);
+
+		// Set the position of the window to the top left corner.
+		posX = posY = 0;
+	}
+	else
+	{
+		// If windowed then set it to 800x600 resolution.
+		screenWidth = 800;
+		screenHeight = 600;
+
+		// Place the window in the middle of the screen.
+		posX = (GetSystemMetrics(SM_CXSCREEN) - screenWidth) / 2;
+		posY = (GetSystemMetrics(SM_CYSCREEN) - screenHeight) / 2;
+	}
+
+	// Create the window with the screen settings and get the handle to it.
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+		applicationName,
+		applicationName,
+		WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_CAPTION | WS_SYSMENU,
+		posX,
+		posY,
+		screenWidth,
+		screenHeight,
+		nullptr,
+		nullptr,
+		hinstance,
+		nullptr);
+
+	// Bring the window up on the screen and set it as main focus.
+	ShowWindow(hwnd, SW_SHOW);
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+
+	// Hide the mouse cursor.
+	ShowCursor(false);
+
+	return;
+}
+
+void System::ShutdownWindows()
+{
+	// Show the mouse cursor.
+	ShowCursor(true);
+
+	// Fix the display settings if leaving full screen mode.
+	if (FULL_SCREEN)
+	{
+		ChangeDisplaySettings(NULL, 0);
+	}
+
+	// Remove the window.
+	DestroyWindow(hwnd);
+	hwnd = NULL;
+
+	// Remove the application instance.
+	UnregisterClass(applicationName, hinstance);
+	hinstance = NULL;
+
+	// Release the pointer to this class.
+	ApplicationHandle = NULL;
+
+	return;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_CLOSE:
+	{
+					 PostQuitMessage(0);
+					 return 0;
+	}
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
