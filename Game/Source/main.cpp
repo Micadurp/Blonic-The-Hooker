1d6716c4555312c6f98d04cf43da0d05e89a9b05
Micadurp <neoswed@hotmail.com>
2015-04-02 09:03:47 +0200

    Added some basics for handling windows, makes an empty window currently

--- Game/Source/main.cpp
+++ Game/Source/main.cpp
@@ -1,13 +1,30 @@
-#include <Windows.h>
+#include "System.h"
 
-#include <iostream>
-using namespace std;
 
-int main()
-
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
-	cout << "hej" << endl;
-	system("pause");
+	System* theSystem;
+	bool result;
+
+
+	// Create the system object.
+	theSystem = new System;
+	if (!theSystem)
+	{
+		return 0;
+	}
+
+	// Initialize and run the system object.
+	result = theSystem->Initialize();
+	if (result)
+	{
+		theSystem->Run();
+	}
+
+	// Shutdown and release the system object.
+	theSystem->Shutdown();
+	delete theSystem;
+	theSystem = 0;
 
 	return 0;
 }
