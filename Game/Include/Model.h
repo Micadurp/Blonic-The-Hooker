825eddefa8b5561ed4826bf513c83adafe3a33d4
Micadurp <neoswed@hotmail.com>
2015-04-16 17:17:27 +0200

    fixade lite obj läsning i model

--- Game/Include/Model.h
+++ Game/Include/Model.h
@@ -16,10 +16,18 @@
 class Model
 {
 protected:
+	struct VertexType
+	{
+		DirectX::XMFLOAT4 position;
+		DirectX::XMFLOAT2 texture;
+		DirectX::XMFLOAT3 normal;
+	};
 	
 	ID3D11Buffer* meshVertexBuff;
+	ID3D11Buffer* meshIndexBuff;
+	int indexCount;
 
-	ID3D11ShaderResourceView * textureShaderResource;
+	ID3D11ShaderResourceView* textureShaderResource;
 	std::wstring textureName;
 
 	struct Vertex
@@ -45,8 +53,8 @@ protected:
 
 	int nrOfVertices;
 
-
-	UINT32 vertexSize = sizeof(float)* 5;
+	//change
+	UINT32 vertexSize = sizeof(VertexType);
 
 	ID3D11Buffer*   geometryShaderCB;
 	D3D11_BUFFER_DESC geometryShaderDesc;
@@ -54,10 +62,12 @@ protected:
 	D3D11_SUBRESOURCE_DATA geometryShaderData;
 public:
 	Model();
-	Model(std::string modelName);
+	Model(std::wstring modelName, ID3D11Device* device);
 	~Model();
 
 	virtual void Update();
-	void Render();
+	void Render(ID3D11DeviceContext* deviceContext);
+
+	bool LoadObj(std::wstring filename, ID3D11Device* device);
 };
 #endif
