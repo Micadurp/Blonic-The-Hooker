17ed9b7b65b9cf5dd1798933092cf6754a84ec59
Micadurp <neoswed@hotmail.com>
2015-04-16 14:07:36 +0200

    added base for gameplay

---
+++ Game/Include/GamePlay.h
@@ -0,0 +1,16 @@
+#ifndef GAMEPLAY_H
+#define GAMEPLAY_H
+
+class GamePlay
+{
+private:
+
+public:
+	GamePlay();
+	virtual ~GamePlay();
+
+	void Update();
+	void Render();
+};
+
+#endif
---
+++ Game/Source/GamePlay.cpp
@@ -0,0 +1,21 @@
+#include "GamePlay.h"
+
+
+GamePlay::GamePlay()
+{
+
+}
+GamePlay::~GamePlay()
+{
+
+}
+
+void GamePlay::Update()
+{
+
+}
+
+void GamePlay::Render()
+{
+
+}
