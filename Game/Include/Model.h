df73aca23bde7278e498109e7ef2047c77cf2269
LK <maxen940393@gmail.com>
2015-04-16 15:52:30 +0200

    Changes to Model class

---
+++ Game/Include/Model.h
@@ -0,0 +1,62 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+#include <fstream>
+#include <istream>
+#include <sstream>
+
+#include <iostream>
+#include <vector>
+
+
+class Model
+{
+protected:
+	
+	ID3D11Buffer* meshVertexBuff;
+
+	ID3D11ShaderResourceView * textureShaderResource;
+	std::wstring textureName;
+
+	struct Vertex
+	{
+		float x, y, z;
+		float U, V;
+
+		Vertex()
+		{
+
+		}
+		Vertex(float x, float y, float z, float U, float V)
+		{
+			this->x = x;
+			this->y = y;
+			this->z = z;
+
+			this->U = U;
+			this->V = V;
+
+		}
+	};
+
+	int nrOfVertices;
+
+
+	UINT32 vertexSize = sizeof(float)* 5;
+
+	ID3D11Buffer*   geometryShaderCB;
+	D3D11_BUFFER_DESC geometryShaderDesc;
+
+	D3D11_SUBRESOURCE_DATA geometryShaderData;
+public:
+	Model();
+	Model(std::string modelName);
+	~Model();
+
+
+};
+#endif
