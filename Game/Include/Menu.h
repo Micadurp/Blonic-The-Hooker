d8cc4ca68157e65fc5011c05636f504654a8c22b
LK <maxen940393@gmail.com>
2015-04-16 15:25:15 +0200

    Max projekt

---
+++ Game/Include/Menu.h
@@ -0,0 +1,18 @@
+#ifndef MENU_H
+#define MENU_H
+
+
+
+class Menu
+{
+private:
+
+public:
+	Menu();
+	~Menu();
+
+	int Update();
+
+	void Render();
+};
+#endif
---
+++ Game/Source/Menu.cpp
@@ -0,0 +1,21 @@
+#include"Menu.h"
+
+Menu::Menu()
+{
+
+}
+
+Menu::~Menu()
+{
+
+}
+
+int Menu::Update()
+{
+
+}
+
+void Menu::Render()
+{
+
+}
