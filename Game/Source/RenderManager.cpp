bf64541a93f70fc089914ce28a2769bc159f138e
unknown <nide11@H470-0006.students.ad.bth.se>
2015-04-17 09:59:19 +0200

    Fixed hlsl files with cmake, daaang

--- Game/Source/RenderManager.cpp
+++ Game/Source/RenderManager.cpp
@@ -1,38 +1,38 @@
 #include"RenderManager.h"
 
-RenderManager::RenderManager()
+RenderManager::RenderManager(ID3D11Device* device)
 {
 #pragma region Create Basic Shaders
 	
 	//create vertex shader
-	//ID3DBlob* pVS = nullptr;
-	//D3DCompileFromFile(L"BasicModelVertexShader.hlsl", nullptr, nullptr, "VS_main", "vs_4_0", 0, NULL, &pVS, nullptr);
+	ID3DBlob* pVS = nullptr;
+	D3DCompileFromFile(L"BasicModelVertexShader.hlsl", nullptr, nullptr, "VS_main", "vs_5_0", 0, NULL, &pVS, nullptr);
 
-	////gDevice->CreateVertexShader(pVS->GetBufferPointer(), pVS->GetBufferSize(), nullptr, &basicModelVertexShader);
+	//gDevice->CreateVertexShader(pVS->GetBufferPointer(), pVS->GetBufferSize(), nullptr, &basicModelVertexShader);
 
-	////create input layout (verified using vertex shader)
-	//D3D11_INPUT_ELEMENT_DESC inputDesc[] = {
-	//	{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-	//	{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	//create input layout (verified using vertex shader)
+	D3D11_INPUT_ELEMENT_DESC inputDesc[] = {
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 
 
-	//};
-	//gDevice->CreateInputLayout(inputDesc, ARRAYSIZE(inputDesc), pVS->GetBufferPointer(), pVS->GetBufferSize(), &basicModelVertexLayout);
-	//pVS->Release();
+	};
+	device->CreateInputLayout(inputDesc, ARRAYSIZE(inputDesc), pVS->GetBufferPointer(), pVS->GetBufferSize(), &basicModelVertexLayout);
+	pVS->Release();
 
-	////create Geometry shader
-	//ID3DBlob* pGS = nullptr;
-	//D3DCompileFromFile(L"BasicModelGeometryShader.hlsl", NULL, nullptr, "GS_main", "gs_4_0", 0, NULL, &pGS, nullptr); 
+	//create Geometry shader
+	ID3DBlob* pGS = nullptr;
+	D3DCompileFromFile(L"BasicModelGeometryShader.hlsl", NULL, nullptr, "GS_main", "gs_5_0", 0, NULL, &pGS, nullptr); 
 
-	//gDevice->CreateGeometryShader(pGS->GetBufferPointer(), pGS->GetBufferSize(), nullptr, &basicModelGeometryShader);
-	//pGS->Release();
+	device->CreateGeometryShader(pGS->GetBufferPointer(), pGS->GetBufferSize(), nullptr, &basicModelGeometryShader);
+	pGS->Release();
 
-	////create pixel shader
-	//ID3DBlob* pPS = nullptr;
-	//D3DCompileFromFile(L"BasicModelPixelShader.hlsl", NULL, nullptr, "PS_main", "ps_4_0", 0, NULL, &pPS, nullptr);
+	//create pixel shader
+	ID3DBlob* pPS = nullptr;
+	D3DCompileFromFile(L"BasicModelPixelShader.hlsl", NULL, nullptr, "PS_main", "ps_5_0", 0, NULL, &pPS, nullptr);
 
-	//gDevice->CreatePixelShader(pPS->GetBufferPointer(), pPS->GetBufferSize(), nullptr, &basicModelPixelShader);
-	//pPS->Release();
+	device->CreatePixelShader(pPS->GetBufferPointer(), pPS->GetBufferSize(), nullptr, &basicModelPixelShader);
+	pPS->Release();
 
 #pragma endregion
 
