6f466bf827308c3d949ef6461ac38fb46d7f13ad
LK <maxen940393@gmail.com>
2015-04-17 15:54:27 +0200

    DDSLOADER

---
+++ Game/Include/Button.h
@@ -0,0 +1,19 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include"Model.h"
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+class Button: Model
+{
+private:
+	
+public:
+	Button();
+	~Button();
+};
+
+#endif
